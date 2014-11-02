b26f45a8bc8ead098057aafadc8cb2e2d36327b3
Christopher T Small <metasoarous@gmail.com>
2014-11-02 12:08:51 -0800

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to block-puzzle
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/block_puzzle/core_test.clj
@@ -0,0 +1,7 @@
+(ns block-puzzle.core-test
+  (:require [clojure.test :refer :all]
+            [block-puzzle.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
