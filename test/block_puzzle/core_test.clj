3458ef7e8343e4a0cbd9cf87ae8164c39b81eac4
Christopher T Small <metasoarous@gmail.com>
2014-11-07 08:38:33 -0800

    Have tests running for directions stuff

--- test/block_puzzle/core_test.clj
+++ test/block_puzzle/core_test.clj
@@ -2,6 +2,21 @@
   (:require [clojure.test :refer :all]
             [block-puzzle.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest penult-test
+  (is (= (penult [1 2 3 4 5]) 4)))
+
+(deftest last-direction-test
+  (testing "simple example"
+    (is (= (last-direction [[0 0 1] [0 0 2]]) [0 0 -1]))))
+
+(deftest new-directions-test
+  (testing "0 length path should give single direction"
+    (is (= (count (new-directions []))
+           1)))
+  (testing "something with only one move having been made should give only one direction"
+    (is (= (count (new-directions [[0 0 1] [0 0 2]]))
+           1)))
+  (testing "new-directions on longer paths"
+    (let [new-dirs (new-directions [9 9 9 9 [1 0 0] [2 0 0]])]
+      (is (= (count new-dirs) 4))
+      (is (= (set new-dirs) (set [[0 1 0] [0 -1 0] [0 0 1] [0 0 -1]]))))))
